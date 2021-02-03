7e53ab3e4e200a66417a4ad9a1dffdd6b14de4db
breakfastitem <andrew.ehrman@gmail.com>
2021-02-02 21:12:11 -0500

    Added all routes without objects

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,4 +2,12 @@ module.exports = function(app) {
     app.get("/test", function(req,res){
         res.render("test",{});
     });
+
+    app.get("/movie/:movieId", function(req,res){
+        res.render("movieDiscussion",{});
+    });
+
+    app.get("/view/:viewId",function(req,res){
+        res.render("viewParty",{});
+    });
 }
