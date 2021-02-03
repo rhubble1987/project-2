3852b73a0663dd37645fe76aab9e7f46f2bbcfb1
Ryan Hubble <rhubble1987@gmail.com>
2021-02-02 19:23:14 -0500

    moved some depenencies inside the function for parties controller

--- controllers/partiesController.js
+++ controllers/partiesController.js
@@ -1,8 +1,9 @@
 const db = require('../models');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
 
 module.exports = function(app) {
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 //Loading index page will pull all saved view parties
 app.get("/", function(req,res) {
