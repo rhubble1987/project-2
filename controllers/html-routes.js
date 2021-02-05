c8d98dcf4e9fab83160bb77722335ad24dbb96b4
breakfastitem <andrew.ehrman@gmail.com>
2021-02-04 21:17:05 -0500

    added view route and removed test route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,41 +1,40 @@
 const db = require('../models');
 
-module.exports = function(app) {
-    app.get("/",function(req,res){
+module.exports = function (app) {
+    app.get("/", function (req, res) {
 
-        res.render("index",{});
+        res.render("index", {});
 
     });
-
-    app.get("/test", function(req,res){
-        res.render("test",{});
-    });
-
-    app.get("/movie/:movieId", function(req,res){
-        db.ViewParty.findAll({where: {OMDBId: req.params.movieId}}).then(function(data) {
+    app.get("/movie/:movieId", function (req, res) {
+        db.ViewParty.findAll({ where: { OMDBId: req.params.movieId } }).then(function (data) {
             const hbsObject = {
-                OMDBId: req.params.movieId,
                 parties: data
             };
             console.log(hbsObject);
-            res.render("movieDiscussion",hbsObject);
-        }).catch(function(err) {
+            res.render("movieDiscussion", hbsObject);
+        }).catch(function (err) {
             const hbsObject = {
-                OMDBId: req.params.movieId,
                 parties: []
             };
-            res.render("movieDiscussion",hbsObject);
+            res.render("movieDiscussion", hbsObject);
         });
     });
 
-    app.get("/view/:viewId",function(req,res){
-        db.ViewParty.findAll(req.params.id,{include:Chat}).then(function(data) {
+    app.get("/view/:viewId", function (req, res) {
+        db.Chat.findAll({ where: { ViewPartyId: req.params.viewId } }).then(function (data) {
             const hbsObject = {
-                party: data
+                chats: data
             };
-            console.log(data.chatlogs);
-            res.render("viewParty",hbsObject);
+            console.log(data);
+            res.render("viewParty", hbsObject);
+        }).catch(function (err) {
+            const hbsObject = {
+                chats: []
+            };
+            console.log(data);
+            res.render("viewParty", hbsObject);
+        });
     });
-});
 
 }
