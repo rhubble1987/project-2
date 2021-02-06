2c364afe8afe6c053bcb35b5ba3b9e70c0de2c11
breakfastitem <andrew.ehrman@gmail.com>
2021-02-06 01:48:20 -0500

    Added post route for  adding chats to database

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,43 +1,52 @@
 const db = require('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
 
-app.post("/api/parties", function (req,res) {
-  
-    db.ViewParty.create({
-        OMDBId: req.body.OMDBId,
-        roomName: req.body.roomName,
-        viewerNumber: 0,
-        // viewDay: req.body.viewDay,
-        // viewTime: req.body.viewTime
-    }).then(function(dbViewParty) {
-        res.json(dbViewParty);
+    app.post("/api/parties", function (req, res) {
+
+        db.ViewParty.create({
+            OMDBId: req.body.OMDBId,
+            roomName: req.body.roomName,
+            viewerNumber: 0,
+            // viewDay: req.body.viewDay,
+            // viewTime: req.body.viewTime
+        }).then(function (dbViewParty) {
+            res.json(dbViewParty);
+        });
     });
-});
 
-app.delete("/api/parties/:id", function(req,res) {
-    db.ViewParty.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(dbViewParty) {
-        res.json(dbViewParty);
+    app.delete("/api/parties/:id", function (req, res) {
+        db.ViewParty.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbViewParty) {
+            res.json(dbViewParty);
+        });
     });
-});
 
-app.put("/api/parties/:id", function (req,res) {
-    db.ViewParty.update({
-        viewerNumber: 0,
-        // viewDay: req.body.viewDay,
-        // viewTime: req.body.viewTime
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then(function(dbViewParty) {
-        res.json(dbViewParty);
+    app.put("/api/parties/:id", function (req, res) {
+        db.ViewParty.update({
+            viewerNumber: 0,
+            // viewDay: req.body.viewDay,
+            // viewTime: req.body.viewTime
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbViewParty) {
+            res.json(dbViewParty);
+        });
     });
-});
+
+    app.post("/api/chats", function (req, res) {
+        db.chats.create({
+            author: null,
+            body: req.body.message
+        }).then(function (dbViewParty) {
+            res.json(dbViewParty);
+        });
+    });
 
 }
